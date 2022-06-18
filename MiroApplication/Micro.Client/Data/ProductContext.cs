d73d36e52937411c1a9404b0b0a228190b7665b0
Muneendra <muneendragoud@gmail.com>
2022-06-18 22:25:36 +0530

    updated

---
+++ MiroApplication/Micro.Client/Data/ProductContext.cs
@@ -0,0 +1,36 @@
+﻿using Micro.Client.Models;
+
+namespace Micro.Client.Data
+{
+    public class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product()
+            {
+            Name="Phonexl",
+            Description="This phone flagh support",
+            ImageFile="Product-1.png",
+            Price=950.00M,
+            Category="Smart-Phone"
+            },
+            new Product()
+            {
+                Name="samsung-10",
+            Description="This phone flagh support",
+            ImageFile="Product-2.png",
+            Price=850.00M,
+            Category="Smart-Phone"
+            },
+            new Product()
+            {
+            Name="samsung-1",
+            Description="This phone flagh support",
+            ImageFile="Product-2.png",
+            Price=850.00M,
+            Category="Smart-Phone"
+            }
+
+        };
+    }
+}
---
+++ MiroApplication/MicroCart.Client/Data/ProductContext.cs
@@ -0,0 +1,36 @@
+﻿using MicroCart.Client.Models;
+
+namespace MicroCart.Client.Data
+{
+    public class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product()
+            {
+            Name="Phonexl",
+            Description="This phone flagrrrh support",
+            ImageFile="Product-1.png",
+            Price=950.00M,
+            Category="Smart-Phone"
+            },
+            new Product()
+            {
+                Name="samsung-10",
+            Description="This phone flagh support evvertyuje",
+            ImageFile="Product-2.png",
+            Price=850.00M,
+            Category="Smart-Phone"
+            },
+            new Product()
+            {
+            Name="samsung-1",
+            Description="This phone flagh supffffport",
+            ImageFile="Product-2.png",
+            Price=850.00M,
+            Category="Smart-Phone"
+            }
+
+        };
+    }
+}
